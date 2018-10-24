ff457bde0d8c0e14fbcb705bd403b178815e7780
alevinas <alefv.97@gmail.com>
2018-10-23 22:21:39 -0300

    Agrego los contadores cuando compila bien

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ extern int yynerrs;
 int main() {
 	switch( yyparse() ){
 	case 0:
-		puts("Compilacion terminada con exito"); return 0;
+		puts("Compilacion terminada con exito"); printf("Errores sintácticos: %d - Errores léxicos: %d\n",yynerrs,errlex); return 0;
 	case 1:
 		puts("Errores de compilación");	printf("Errores sintácticos: %d - Errores léxicos: %d\n",yynerrs,errlex); return 1;
 	case 2:
@@ -31,7 +31,6 @@ int main() {
 	}
 	return 0;
 }
-
 
 
 /* Informa la ocurrencia de un error. */
